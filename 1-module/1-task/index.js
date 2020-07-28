e6b2aeeb14732ee33781efd137b62f77d2b3a184
Red-Warrior <sword_and_brush@mail.ru>
2020-07-28 20:28:53 +0300

    Упрощение кода

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,16 +4,14 @@
  * @returns {number}
  */
 function factorial(n) {
-  if (isNaN(n) || n < 0) {
-    return "Введите натуральное число";
-  } else if (n === 0) {
+  if (n === 0) {
     return 1;
-  } else {
-    let total = n;
-    for (; n > 1; n--) {
-      total *= n - 1;
-    }
-    return total;
   }
+  let total = n;
+  for (; n > 1; n--) {
+    total *= n - 1;
+  }
+  return total;
+
 }
 
