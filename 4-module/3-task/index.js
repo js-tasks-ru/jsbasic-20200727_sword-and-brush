4167d74c8b32945c0b4111a4aaa43e9d5db56001
Red-Warrior <sword_and_brush@mail.ru>
2020-08-12 23:56:53 +0300

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,25 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let i = 0; i < table.rows.length; i++) {
+    if (table.rows[i].cells[3].dataset.available === "true") {
+      table.rows[i].classList.add("available");
+
+    } else if (table.rows[i].cells[3].dataset.available === "false") {
+      table.rows[i].classList.add("unavailable")
+
+    } else if (!table.rows[i].cells[3].hasAttribute("[dataset-available]") && table.rows[i] !== table.rows[0]) {
+      table.rows[i].hidden = true;
+    }
+
+    if (table.rows[i].cells[2].innerHTML === "m") {
+      table.rows[i].classList.add("male");
+
+    } else if (table.rows[i].cells[2].innerHTML === "f") {
+      table.rows[i].classList.add("female");
+    }
+    if (+(table.rows[i].cells[1].innerHTML) < 18) {
+      table.rows[i].style.textDecoration = "line-through";
+    }
+  }
 }
