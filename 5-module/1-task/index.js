e16705914d51a4579b9edfa04229345d0aafbba0
Red-Warrior <sword_and_brush@mail.ru>
2020-08-19 00:31:41 +0300

    Функция скрыть элемент

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,5 @@
 function hideSelf() {
-  // ваш код...
+  let targetElement = document.querySelector(".hide-self-button");
+  let visibility = () => targetElement.hidden = true;
+  targetElement.addEventListener("click", visibility);
 }
