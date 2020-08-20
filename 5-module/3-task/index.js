cd8ac9b9ea7b86f1d3430069641fb24142090dc2
Red-Warrior <sword_and_brush@mail.ru>
2020-08-20 23:36:46 +0300

    Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+
+  let value = 0;
+  let counter = document.querySelector(".carousel__inner").offsetWidth;
+  let slider = document.querySelector(".carousel__inner");
+  let left = document.querySelector(".carousel__arrow_left");
+  let right = document.querySelector(".carousel__arrow_right");
+
+  left.style.display = "none";
+
+  let slideRight = () => {
+    slider.style.transform = `translateX(${value -= counter}px)`;
+
+    if (value === -(counter * 3)) {
+      right.style.display = "none"
+    }
+
+    if (value < 0) {
+      left.style.display = "";
+    }
+  }
+
+  let slideLeft = () => {
+    slider.style.transform = `translateX(${value += counter}px)`;
+
+    if (value > -(counter * 3)) {
+      right.style.display = "";
+    }
+    if (value === 0) {
+      left.style.display = "none";
+    }
+  }
+
+  right.addEventListener("click", slideRight);
+  left.addEventListener("click", slideLeft);
 }
+
+
